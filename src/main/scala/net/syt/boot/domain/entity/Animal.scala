d0c2c3d02f34f4295763f408cd8ab39344a89f93
Yutao SUN <yutao.sun.fr@gmail.com>
2020-01-15 23:25:48 +0100

    Update Animal.scala

--- src/main/scala/net/syt/boot/domain/entity/Animal.scala
+++ src/main/scala/net/syt/boot/domain/entity/Animal.scala
@@ -2,7 +2,9 @@ package net.syt.boot.domain.entity
 
 /**
  * ADT, no operation or method should be included,
- * Animal is a Product type
+ * Animal is a Sum type
+ * While Dog or Bird could be Product type
+ * Indeed, here the Animal is a Hybrid type :)
  */
 sealed trait Animal
 
